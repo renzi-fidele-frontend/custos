cb0270a1fc7e2381107f5cfd2406fc00915f3a53
Renzi Fidele <renzifidele2001@gmail.com>
2023-06-19 04:25:16 +0200

    Responsivo pronto

--- src/pages/NovoProjeto/NovoProjeto.jsx
+++ src/pages/NovoProjeto/NovoProjeto.jsx
@@ -7,6 +7,7 @@ import { Timestamp, addDoc, collection, doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase/config";
 import { AuthValue } from "../../context/AuthContext";
 import spinner from "../../images/spin.svg";
+import foto from "../../images/criarProjeto.svg"
 
 function NovoProjeto() {
     //  Usando o useNavigate
@@ -76,88 +77,90 @@ function NovoProjeto() {
                     <div id={styles.align}>
                         <h2>Criar projeto</h2>
                         <p>Crie seus projetos para depois adicionar os serviços</p>
-
-                        <form
-                            onSubmit={(e) => {
-                                e.preventDefault();
-                                send();
-                            }}
-                            id={styles.formulario}
-                        >
-                            <div className={styles.afastar}>
-                                <label className={styles.label} htmlFor="nomeProjeto">
-                                    Nome do projeto:
-                                </label>
-                                <motion.input
-                                    whileFocus={{ backgroundColor: "#ffe14d" }}
-                                    transition={{ duration: 0.5 }}
-                                    type="text"
-                                    name="nomeProjeto"
-                                    id="nomeProjeto"
-                                    placeholder="Insira o nome do projeto"
-                                    className={styles.input}
-                                    onChange={(e) => {
-                                        setNome(e.target.value);
-                                    }}
-                                    value={nome}
-                                    required
-                                />
-                            </div>
-                            <div className={styles.afastar}>
-                                <label className={styles.label} htmlFor="orcamento">
-                                    Orçamento do projeto:
-                                </label>
-                                <motion.input
-                                    whileFocus={{ backgroundColor: "#ffe14d" }}
-                                    transition={{ duration: 0.5 }}
-                                    type="number"
-                                    name="orcamento"
-                                    id="orcamento"
-                                    onChange={(e) => {
-                                        setorcamento(e.target.value);
-                                    }}
-                                    value={orcamento}
-                                    placeholder="Insira o orçamento total"
-                                    className={styles.input}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="category_id" className={styles.label}>
-                                    Selecione a categoria:
-                                </label>
-                                <motion.select
-                                    whileTap={{ backgroundColor: "#faf7dd" }}
-                                    transition={{ duration: 0.5 }}
-                                    onChange={(e) => {
-                                        categorias.forEach((v) => {
-                                            if (e.target.options[e.target.selectedIndex].text === v.nome) {
-                                                console.log(v);
-                                                setCategoria(v);
-                                            }
-                                        });
-                                    }}
-                                    className={styles.select}
-                                    name="category_id"
-                                >
-                                    <option disabled selected>
-                                        Selecione a categoria
-                                    </option>
-                                    {categorias.map((e) => {
-                                        return (
-                                            <option value={e.id} key={e.id}>
-                                                {e.nome}
-                                            </option>
-                                        );
-                                    })}
-                                </motion.select>
-                            </div>
-                            <div>
-                                <motion.button whileHover={{ scale: 1.2, color: "#ffbb33" }} id={styles.botao} type="submit">
-                                    Criar projeto
-                                </motion.button>
-                            </div>
-                        </form>
+                        <div id={styles.linha}>
+                            <form
+                                onSubmit={(e) => {
+                                    e.preventDefault();
+                                    send();
+                                }}
+                                id={styles.formulario}
+                            >
+                                <div className={styles.afastar}>
+                                    <label className={styles.label} htmlFor="nomeProjeto">
+                                        Nome do projeto:
+                                    </label>
+                                    <motion.input
+                                        whileFocus={{ backgroundColor: "#ffe14d" }}
+                                        transition={{ duration: 0.5 }}
+                                        type="text"
+                                        name="nomeProjeto"
+                                        id="nomeProjeto"
+                                        placeholder="Insira o nome do projeto"
+                                        className={styles.input}
+                                        onChange={(e) => {
+                                            setNome(e.target.value);
+                                        }}
+                                        value={nome}
+                                        required
+                                    />
+                                </div>
+                                <div className={styles.afastar}>
+                                    <label className={styles.label} htmlFor="orcamento">
+                                        Orçamento do projeto:
+                                    </label>
+                                    <motion.input
+                                        whileFocus={{ backgroundColor: "#ffe14d" }}
+                                        transition={{ duration: 0.5 }}
+                                        type="number"
+                                        name="orcamento"
+                                        id="orcamento"
+                                        onChange={(e) => {
+                                            setorcamento(e.target.value);
+                                        }}
+                                        value={orcamento}
+                                        placeholder="Insira o orçamento total"
+                                        className={styles.input}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="category_id" className={styles.label}>
+                                        Selecione a categoria:
+                                    </label>
+                                    <motion.select
+                                        whileTap={{ backgroundColor: "#faf7dd" }}
+                                        transition={{ duration: 0.5 }}
+                                        onChange={(e) => {
+                                            categorias.forEach((v) => {
+                                                if (e.target.options[e.target.selectedIndex].text === v.nome) {
+                                                    console.log(v);
+                                                    setCategoria(v);
+                                                }
+                                            });
+                                        }}
+                                        className={styles.select}
+                                        name="category_id"
+                                    >
+                                        <option disabled selected>
+                                            Selecione a categoria
+                                        </option>
+                                        {categorias.map((e) => {
+                                            return (
+                                                <option value={e.id} key={e.id}>
+                                                    {e.nome}
+                                                </option>
+                                            );
+                                        })}
+                                    </motion.select>
+                                </div>
+                                <div>
+                                    <motion.button whileHover={{ scale: 1.2, color: "#ffbb33" }} id={styles.botao} type="submit">
+                                        Criar projeto
+                                    </motion.button>
+                                </div>
+                            </form>
+                            <img src={foto} alt="ilustracao para criar projeto" />
+                        </div>
                     </div>
                 </motion.section>
             ) : (
